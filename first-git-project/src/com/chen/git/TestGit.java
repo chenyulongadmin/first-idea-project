75c7e636a2227a7d3c5ca91db7ef6391aa587931
chenyulong <575877305@qq.com>
2020-07-19 21:17:23 +0800

    chen git

---
+++ first-git-project/src/com/chen/git/TestGit.java
@@ -0,0 +1,8 @@
+package com.chen.git;
+
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hellow git");
+        System.out.println("hellow git");
+    }
+}
