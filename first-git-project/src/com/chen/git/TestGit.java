97629f2623e79a919d0f4817a55625a66e7ef8ec
chenyulong <575877305@qq.com>
2020-07-19 21:20:33 +0800

    chen git

--- first-git-project/src/com/chen/git/TestGit.java
+++ first-git-project/src/com/chen/git/TestGit.java
@@ -4,5 +4,9 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("hellow git");
         System.out.println("hellow git");
+        System.out.println("hellow git");
+        System.out.println("hellow git"); System.out.println("hellow git");
+        System.out.println("hellow git");
+
     }
 }
